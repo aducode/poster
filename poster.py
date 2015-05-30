970cd258001faa38882a9bcae77cc8b19491abc4
issac <aducode@126.com>
2015-05-30 15:26:05 +0800

    add base functions

--- poster.py
+++ poster.py
@@ -4,11 +4,52 @@
 import markdown2
 import sys
 	
-def init(email, password, theme):
+def init():
 	"""
 	init github pages
+	
 	"""
 	pass
+	
+
+def list(title=None, date=None):
+	"""
+	list all the blogs
+	:param title: blog title
+	:param date:  blog created/modified date
+	:return
+	"""
+	pass
+
+def create(title, date=None):
+	"""
+	create a new blog
+	:param title: blog's title
+	:param date:  created date
+	:return
+	"""
+	pass
+
+def edit(title, date=None):
+	"""
+	edit blog
+	:param tilte: blog title
+	:param date: blog created/modified date
+	:return
+	"""
+	pass
+	
+	
+def remove(title, date=None):
+	"""
+	remove blog
+	:param title: blog's title
+	:param date: blog created/modified date
+	:return
+	"""
+	pass
+	
+
 	
 
 if __name__ == '__main__':
