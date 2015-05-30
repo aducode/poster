8173891b9e8e424a9634f5c77e85b6d30547a24f
issac <aducode@126.com>
2015-05-30 15:15:02 +0800

    fix

--- poster.py
+++ poster.py
@@ -2,3 +2,16 @@
 # -*- coding:utf-8 -*-
 """A simple poster for use Github Pages as Blog"""
 import markdown2
+import sys
+	
+def init(email, password, theme):
+	"""
+	init github pages
+	"""
+	pass
+	
+
+if __name__ == '__main__':
+	pass
+
+	
